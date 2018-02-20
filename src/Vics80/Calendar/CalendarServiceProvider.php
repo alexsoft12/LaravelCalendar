991c501615864e7ccd916e44f8ed195d23d815c3
ITwrx <ITwrx@users.noreply.github.com>
2018-02-20 11:29:52 -0600

    Update CalendarServiceProvider.php
    use singleton for laravel 5.5

--- src/Vics80/Calendar/CalendarServiceProvider.php
+++ src/Vics80/Calendar/CalendarServiceProvider.php
@@ -4,35 +4,37 @@ use Illuminate\Support\ServiceProvider;
 
 class CalendarServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['calendar'] = $this->app->share(function($app)
-		{
-			$request = $app['request'];
-			return new CalendarGenerator($request);
-		});
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //$this->app['calendar'] = $this->app->share(function($app)
+        $this->app->singleton('calendar', function($app) {
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+            $request = $app['request'];
+            return new CalendarGenerator($request);
+        });
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
